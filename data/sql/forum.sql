cc85cdcd374bde6d841c3da0053ffe2600926377
JekaMell <55jeka@gmail.com>
2014-06-27 23:44:37 +0300

    Update: database dump

--- data/sql/forum.sql
+++ data/sql/forum.sql
@@ -26,6 +26,10 @@ CREATE TABLE `category` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL DEFAULT '',
   `description` text,
+  `content` varchar(255) DEFAULT NULL,
+  `date_add` datetime DEFAULT NULL,
+  `id_author` bigint(20) DEFAULT NULL,
+  `id_category` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -36,7 +40,7 @@ CREATE TABLE `category` (
 
 LOCK TABLES `category` WRITE;
 /*!40000 ALTER TABLE `category` DISABLE KEYS */;
-INSERT INTO `category` VALUES (1,'Announcements','Announcements and general project news'),(2,'Container','Discussion on Spring\'s dependency injection container, including XML and annotation-based configuration.'),(3,'Data','Discussion on data access with Spring. This forum is for Spring Data Community Extensions like Solr, Elasticsearch, Couchbase, FuzzyDB.'),(4,'Security','Discussing on securing applications with Spring Security and its extensions.'),(5,'Web','Discussion on developing web applications with Spring, including usage of Spring MVC and its REST support, as well as the HATEOAS, Web Flow, and Social projects.');
+INSERT INTO `category` VALUES (1,'Announcements','Announcements and general project news',NULL,NULL,NULL,NULL),(2,'Container','Discussion on Spring\'s dependency injection container, including XML and annotation-based configuration.',NULL,NULL,NULL,NULL),(3,'Data','Discussion on data access with Spring. This forum is for Spring Data Community Extensions like Solr, Elasticsearch, Couchbase, FuzzyDB.',NULL,NULL,NULL,NULL),(4,'Security','Discussing on securing applications with Spring Security and its extensions.',NULL,NULL,NULL,NULL),(5,'Web','Discussion on developing web applications with Spring, including usage of Spring MVC and its REST support, as well as the HATEOAS, Web Flow, and Social projects.',NULL,NULL,NULL,NULL);
 /*!40000 ALTER TABLE `category` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -52,12 +56,13 @@ CREATE TABLE `comment` (
   `content` text CHARACTER SET utf8 NOT NULL,
   `id_topic` int(11) NOT NULL,
   `id_author` int(11) NOT NULL DEFAULT '0',
+  `date_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `id_author` (`id_author`),
   KEY `id_topic` (`id_topic`),
   CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`id_author`) REFERENCES `user` (`id`),
   CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`id_topic`) REFERENCES `topic` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,6 +71,7 @@ CREATE TABLE `comment` (
 
 LOCK TABLES `comment` WRITE;
 /*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+INSERT INTO `comment` VALUES (1,'This question is probably a better candidate for StackOverflow, perhaps with the \"spring\" tag.\r\n\r\nCould you post your question there?\r\nThe forums are moving to stackoverflow, a page explaining all this will be available there in a few days: http://spring.io/questions',3,2,'2014-06-27 12:37:01'),(2,'Yes I have the same problem. I think you will need a setter method to support mock injection. In my understanding you don\'t need a testAppContext.xml if you are not making some form of integration testing with real parts (configured in your testAppContext.xml).\r\n\r\nYou probably want to avoid creating Mock implementations of your collaborators to inject them in the testAppContext.xml.\r\n\r\nIf you need only use a mocking framework and directly set the collaborators to perform your unit test then I believe it is preferable to do so.',3,1,'2014-06-27 12:37:01'),(3,'If this is just the test, how are you setting OutsideService in your non-test code if you don\'t have a setter? Are you using the constructor? Are you hardcoding the dependency by using \'new\'?',3,2,'2014-06-27 12:37:01');
 /*!40000 ALTER TABLE `comment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -109,12 +115,13 @@ CREATE TABLE `topic` (
   `date_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `id_author` int(11) NOT NULL DEFAULT '0',
   `id_category` int(11) NOT NULL DEFAULT '0',
+  `category_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `id_author` (`id_author`),
   KEY `id_category` (`id_category`),
   CONSTRAINT `topic_ibfk_1` FOREIGN KEY (`id_author`) REFERENCES `user` (`id`),
   CONSTRAINT `topic_ibfk_2` FOREIGN KEY (`id_category`) REFERENCES `category` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -123,7 +130,7 @@ CREATE TABLE `topic` (
 
 LOCK TABLES `topic` WRITE;
 /*!40000 ALTER TABLE `topic` DISABLE KEYS */;
-INSERT INTO `topic` VALUES (1,'Spring Framework 3.2.5 Released','The fifth maintenance release in the Spring Framework 3.2.x line is now available via Maven Central or the SpringSource repository. This release includes nearly 50 bug fixes and improvements and we recommend that all users upgrade.','2014-06-20 07:51:29',1,1),(2,'Spring Scala 1.0.0.RC1 has been released','Dear Spring community,\r\n\r\nI\'m pleased to announce that Spring Scala 1.0.0.RC1 has been released! This is first release candidate in the release cycle. It contains numerous small changes, mainly to prepare for a 1.0.0.RELEASE.\r\n\r\nPlease give it a shot! As before the jar is available for download at our milestone repository, http://repo.springsource.org/libs-milestone. For Maven users:\r\n\r\n<repositories>\r\n    <repository>\r\n        <id>milestone.repo.springsource.org</id>\r\n        <name>repo.springsource.org-milestone</name>\r\n        <url>https://repo.springsource.org/libs-milestone</url>\r\n    </repository>\r\n</repositories>\r\n<dependency>\r\n    <groupId>org.springframework.scala</groupId>\r\n    <artifactId>spring-scala_2.10</artifactId>\r\n    <version>1.0.0.RC1</version>\r\n</dependency>\r\n\r\nNote that we have started to include the Scala version number in the artifactId, a common practice for Scala libraries. \r\n\r\nThanks,\r\n\r\nArjen','2014-06-20 07:56:56',1,1),(3,'Spring Web Services 2.1.4.RELEASE released','Dear Spring community,\r\n\r\nI\'m pleased to announce that Spring Web Services 2.1.4.RELEASE has been released!\r\n\r\nThis is the latest GA release in the 2.1 release cycle. It mainly consists of bug fixes.\r\n\r\nPlease see the changelog for more details.\r\n\r\nThis release is currently available on our Maven release repository, and will be available on Maven Central and the download page shortly.\r\n\r\nFor more information, refer to \r\n\r\nhttp://static.springframework.org/spring-ws/sites/2.0','2014-06-20 07:57:37',1,1),(4,'ApplicationContext.xml cannot be found when starting Tomcat server','Hi guys,\r\nI know that this question had to be asked about million times but i have tried all suggested solutions for this and none of them have worked. I hope that it is not against rules of this foreum but ionstead of writing it down again i post here my unanswered question from stack overflo \r\n\r\nhttp://stackoverflow.com/questions/2...62254_23008148\r\n\r\nthanks for your help in advance','2014-06-20 07:59:41',1,2),(5,'Help me on this','I am working on existing web project. It is just developed using jsp and servlets and deployed on tomcat.\r\n\r\nWeb app maintainnce configurations in seperate file for configuration parameters. It is like this\r\n\r\ntomcat/conf/app.conf\r\n\r\nNO_WORKING_DYS 10\r\nNO_PEOPLE 5\r\n......\r\n....\r\n\r\n\r\nApp is reading this file in context listner and keep values in a map. Then what happen is, web application needed to be restarted after changing a configuration value. If not, new config value is not picked up. I know it is obvious since we are reading the config file inside the context listener.\r\n\r\n\r\nI need to know is there any possibility to overcome this issue (change config values with out restating or redeploing) using spring\r\nIOC container..\r\n\r\n\r\nThannk you...','2014-06-20 08:03:47',1,2);
+INSERT INTO `topic` VALUES (2,'Spring Framework 3.2.5 Released','The fifth maintenance release in the Spring Framework 3.2.x line is now available via Maven Central or the SpringSource repository. This release includes nearly 50 bug fixes and improvements and we recommend that all users upgrade.','2014-06-27 10:12:06',1,1,NULL),(3,'Mock injection','How can I,\r\n\r\npropagate a mock service created in a unit test? I would like, that spring when the test starts uses as bean injection of the service, the service mock.\r\n\r\nThanks.\r\n\r\nRegards.','2014-06-27 10:16:31',1,2,NULL);
 /*!40000 ALTER TABLE `topic` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -137,7 +144,10 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `login` varchar(255) NOT NULL DEFAULT '',
+  `name_first` varchar(255) DEFAULT NULL,
+  `name_last` varchar(255) DEFAULT NULL,
   `password` varchar(255) NOT NULL DEFAULT '',
+  `skype` varchar(255) DEFAULT '',
   `id_role` int(11) NOT NULL DEFAULT '1',
   `is_enabled` tinyint(4) DEFAULT '1',
   `email` varchar(255) NOT NULL DEFAULT '',
@@ -145,7 +155,7 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   KEY `id_role` (`id_role`),
   CONSTRAINT `user_ibfk_1` FOREIGN KEY (`id_role`) REFERENCES `user_role` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -154,7 +164,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'jekamell','123456',1,1,'55jeka@gmail.com','2014-06-16 15:32:30');
+INSERT INTO `user` VALUES (1,'jekamell','Jeka','Mell','e10adc3949ba59abbe56e057f20f883e','jekamell',1,1,'55jeka@gmail.com','2014-06-27 09:02:15'),(2,'bclozel',NULL,NULL,'e10adc3949ba59abbe56e057f20f883e',NULL,1,1,'bclozel@gmail.com','2014-06-27 10:19:24');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -193,4 +203,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-06-21  0:21:38
+-- Dump completed on 2014-06-27 23:33:50
