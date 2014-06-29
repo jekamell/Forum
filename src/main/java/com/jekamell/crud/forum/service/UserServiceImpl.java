3105136444f30b2eb21151ac01413a6cdc54efef
jekamell <55jeka@gmail.com>
2014-06-29 15:31:33 +0300

    Refactor: services autowiring

--- src/main/java/com/jekamell/crud/forum/service/UserServiceImpl.java
+++ src/main/java/com/jekamell/crud/forum/service/UserServiceImpl.java
@@ -21,7 +21,6 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserDao hibernateUserDao;
-
     @Autowired
     private UserRoleDao hibernateUserRoleDao;
 
